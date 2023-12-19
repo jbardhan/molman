cc403b6a38a77df64a0bddff9603dc2ffd242171
Jaydeep Bardhan <jaydeep.bardhan@pnnl.gov>
2023-12-19 07:26:34 -0800

    Created new test script.  Nothing will work at this point.  Hybrid object now holds a copy of Titratable info, and Molecule set_titratable_group_charges now returns the affected indices and global charge vector

--- mymm/hybrid.py
+++ mymm/hybrid.py
@@ -4,9 +4,8 @@ import mymm
 import copy
 
 class Hybrid:
-    def __init__(self, params):
-        self.params = copy.deepcopy(params)
-        self.titratable = 0
+    def __init__(self, titrationDetails, proteinChargeDistributions, proteinPotentials):
+        self.titrationDetails = copy.deepcopy(titrationDetails)
         
     def writeHybridInputFile(self, filename):
         input_file = open(filename, 'w')
@@ -15,17 +14,17 @@ class Hybrid:
         input_file.write(str(num_titratable_groups))
 
         for i in range(num_titratable_groups):
-            current_residue_info = self.titratable[i]
+###            current_residue_info = self.titratable[i]
             
-            pK_i_model = 0
-            gamma_i    = 0
-            Delta_Delta_G_i = 0
+###            pK_i_model = 0
+###            gamma_i    = 0
+###            Delta_Delta_G_i = 0
 
             group_header_list = [str(x) for x in [K_i_model, gamma_i, Delta_Delta_G_i, i+1]]
             input_file.write("\t".join(group_header_list))
             
             for j in range(num_titratable_groups, start = i+1):
-                Psi_i_j = 0
+###                Psi_i_j = 0
                 
                 input_file.write(str(Psi_i_j))
                 
