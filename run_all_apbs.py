79c5f9c264bc503797f10e1f8973fd9ee0b80171
Jaydeep Bardhan <jaydeep.bardhan@pnnl.gov>
2024-01-02 15:11:20 -0800

    added the --slurm command line option which will submit all APBS jobs to Slurm. tested it on the arg_capped case in testset1

--- run_all_apbs.py
+++ run_all_apbs.py
@@ -2,12 +2,31 @@
 import re, os, sys, subprocess, argparse, logging
 import mymm
 
-def runApbs():
+def runApbs(useSlurm = False):
     ApbsOutput = "apbs.out"
     ApbsCommand = "singularity exec --bind /run/user /tahoma/emsla60376/containers/apbs/apbs_singularity_ubuntu.sif /usr/local/bin/apbs apbs.in"
-    ApbsListForRun = ApbsCommand.split(' ')
-    with open(ApbsOutput,'w') as outfile:
-        subprocess.run(ApbsListForRun, stdout=outfile)
+    
+    if useSlurm:
+        # write Slurm submission script
+        submitScript = 'submit.sh'
+        fh = open(submitScript, 'w')
+        fh.write("#!/bin/bash\n")
+        fh.write("#SBATCH --account=emsla60376\n")
+        fh.write("#SBATCH --partition=normal\n")
+        fh.write("#SBATCH --nodes=1\n")
+        fh.write("#SBATCH --ntasks-per-node=1\n")
+        fh.write("-o %j.out\n")
+        fh.write("-e %j.err\n")
+        fh.write("#SBATCH --time=03:00\n")
+        fh.write(ApbsCommand + " > " + ApbsOutput + "\n")
+        fh.close()
+        # submit Slurm job with sbatch
+        slurmList = ['sbatch', submitScript]
+        subprocess.run(slurmList)
+    else:
+        ApbsListForRun = ApbsCommand.split(' ')
+        with open(ApbsOutput,'w') as outfile:
+            subprocess.run(ApbsListForRun, stdout=outfile)
 
 
 parser = argparse.ArgumentParser()
@@ -18,6 +37,8 @@ parser.add_argument("--titration_list",nargs=1, help="file listing the groups to
 parser.add_argument("--topology", nargs=1, help="CHARMM style topology file--use a PARSE_prot_patch file")
 parser.add_argument("--radii", nargs=1, help="radii.siz file")
 parser.add_argument("--patchfile", nargs=1, help="file listing key patches")
+parser.add_argument("--slurm",default=False,action='store_true')
+
 args = parser.parse_args()
 
 system = mymm.Molecule(PDB=args.pdb[0])
@@ -35,7 +56,7 @@ table.validateTitrationListAgainstMolecule(system)
 
 # Run neutral protein in the starting directory
 print("Got here!")
-runApbs()
+runApbs(args.slurm)
 print("Now here!")
 
 listIndex = 0
@@ -48,11 +69,11 @@ for residue in table.listOfResiduesToTitrate:
         os.chdir(site_plus_state)
 
         os.chdir("protein")
-        runApbs()
+        runApbs(args.slurm)
         os.chdir("..")
 
         os.chdir("model_compound")
-        runApbs()
+        runApbs(args.slurm)
         os.chdir("..")
         os.chdir("..")
             
